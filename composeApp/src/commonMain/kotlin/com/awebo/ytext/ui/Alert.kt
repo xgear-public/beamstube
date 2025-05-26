fa5ac13abd84b0f6795d3984439ba74d60678937
xgear.public <xgear.public@gmail.com>
2025-05-26 21:48:33 +0200

    fix alert layout

--- composeApp/src/commonMain/kotlin/com/awebo/ytext/ui/Alert.kt
+++ composeApp/src/commonMain/kotlin/com/awebo/ytext/ui/Alert.kt
@@ -1,8 +1,8 @@
 package com.awebo.ytext.ui
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material.Button
 import androidx.compose.material.Text
@@ -12,12 +12,16 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun Alert(modifier: Modifier = Modifier, alertText: String, onDismiss: () -> Unit) {
-    Column(modifier.padding(8.dp)) {
+    Column(
+        modifier.padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
         Text(
             modifier = Modifier.widthIn(min = 200.dp, max = 750.dp),
             text = alertText
         )
-        Button(modifier = Modifier.padding(8.dp), onClick = { onDismiss() }) {
+        Button(
+            onClick = { onDismiss() }) {
             Text("Close")
         }
     }
